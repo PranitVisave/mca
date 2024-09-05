af7f79de29c180a6ce5e49835e2dce03353db208
PranitVisave <131594933+PranitVisave@users.noreply.github.com>
2024-09-05 14:29:52 +0530

    Update C-Codes.c

--- C-Codes.c
+++ C-Codes.c
@@ -1,7 +1,7 @@
 //Pranit Visave FYMCA
 //MC24101733
 //Q1
-/*
+
 #include<stdio.h>
 void main(){
     printf("This is my first C program\n");
@@ -9,10 +9,10 @@ void main(){
 
 mca@mca-OptiPlex-3050-AIO:~/Desktop$ ./a.out
 This is my first C program
-*/
+
 
 //Q2
-/*
+
 #include<stdio.h>
 void main(){
     int a,b,c;
@@ -24,9 +24,9 @@ void main(){
 
 mca@mca-OptiPlex-3050-AIO:~/Desktop$ ./a.out
 Sum of A And B : 8
-*/
+
 //Q3
-/*
+
 
 #include<stdio.h>
 void main(){
@@ -61,56 +61,56 @@ void main(){
     printf("Multiplication : %d\n",a*b);
     printf("Division : %d\n",a/b);
 }
-// //Q6-i,ii
-// void main(){
-//     //v= u+at;
-//     float v,u,a,t;
-//     u = 10;
-//     a = 1.6;
-//     t = 20;
-//     v = u + a*t;
-//     printf("Final Velocity : %f",v);
+ //Q6-i,ii
+void main(){
+   //v= u+at;
+    float v,u,a,t;
+     u = 10;
+     a = 1.6;
+     t = 20;
+     v = u + a*t;
+     printf("Final Velocity : %f",v);
 
-//     float s = (u*t) + 1/2 * a*(t*t);
-//     printf("Speed : %f\n",s);
-// }
-// //Q7
-// #include<stdio.h>
-// #include<math.h>
-// void main(){
-//     float p,r,t,s,c;
-//     p = 30000;
-//     r = 6.5;
-//     t = 10;
-//     s = (p*r*t)/100;
-//     printf("Simple Interest : %f\n",s);
-//     //c = p*
-// }
+     float s = (u*t) + 1/2 * a*(t*t);
+    printf("Speed : %f\n",s);
+ }
+//Q7
+#include<stdio.h>
+#include<math.h>
+ void main(){
+     float p,r,t,s,c;
+     p = 30000;
+     r = 6.5;
+     t = 10;
+     s = (p*r*t)/100;
+     printf("Simple Interest : %f\n",s);
+     //c = p*
+ }
 
 // //Q8
-// void main(){
-//     int a=40,b=60;
-//     b=b+a;
-//     a=b-a;
-//     b=b-a;
-//     printf("Original Numbers : a = %d, b=%d\n,a,b");
-//     printf("Swaped Numbers a = %d, b = %d\n",a,b);
-// }
-// //Q9
-// #include<stdio.h>
-// void main(){
-//     int a = 10;
-//     float b = 2.5;
-//     double d = 3.0563;
-//     char c = 'a';
-//     printf("int = %lu",sizeof(a));
-//     printf("float = %lu",sizeof(b));
-//     printf("double = %lu",sizeof(d));
-//     printf("chaR = %lu",sizeof(c));
-// }
-// //Q10
-// #include<stdio.h>
-// void main(){
-//     int a=10,b=20,c=15;
-//     int largest = ((a>b)?(a>c):a:c)
-// }
+ void main(){
+     int a=40,b=60;
+     b=b+a;
+     a=b-a;
+     b=b-a;
+     printf("Original Numbers : a = %d, b=%d\n,a,b");
+     printf("Swaped Numbers a = %d, b = %d\n",a,b);
+ }
+ //Q9
+ #include<stdio.h>
+ void main(){
+     int a = 10;
+     float b = 2.5;
+     double d = 3.0563;
+     char c = 'a';
+     printf("int = %lu",sizeof(a));
+     printf("float = %lu",sizeof(b));
+     printf("double = %lu",sizeof(d));
+     printf("chaR = %lu",sizeof(c));
+ }
+ //Q10
+ #include<stdio.h>
+ void main(){
+     int a=10,b=20,c=15;
+     int largest = ((a>b)?(a>c):a:c)
+ }
