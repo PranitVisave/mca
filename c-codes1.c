ce0e1d415b4ab14cd0577df46360e239afee127c
PranitVisave <131594933+PranitVisave@users.noreply.github.com>
2024-09-05 13:46:26 +0530

    Update c-codes1.c

--- c-codes1.c
+++ c-codes1.c
@@ -233,3 +233,29 @@ void main(){
         printf("\n");
     }
 }
+
+#include<stdio.h>
+void main(){
+        int a[3][3],b[3][3],m[3][3],i,j;
+        printf("Enter Elements in first matrix : ");
+        for (i=0;i<3;i++)
+                for (j=0;i<3;j++)
+                        scanf("%d",&a[i][j]);
+        printf("Enter Elements in second matrix : ");
+        for (i=0;i<3;i++)
+                for (j=0;i<3;j++)
+                        scanf("%d",&b[i][j]);    
+        for (i=0;i<3;i++){
+            for (j=0;i<3;j++){
+                m[i][j]=0;
+                for (int k=0;k<3;k++)
+                    m[i][j] += a[i][j]*b[j][k];
+            }
+        }
+        printf("Multiplication of two matrices : \n");
+        for (i=0;i<3;i++){
+            for (j=0;k<3;j++)
+                printf("%d ",m[i][j]);
+            printf("\n");
+        }
+}
